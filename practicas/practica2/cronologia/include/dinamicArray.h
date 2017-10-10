1bf1a564979553c6cc08f6cac2b751d7b0fb9ccf
Yabir <yabirgb@gmail.com>
2017-10-10 20:12:53 +0200

    Dinamic Array header and body

---
+++ practicas/practica2/cronologia/include/dinamicArray.h
@@ -0,0 +1,29 @@
+#ifndef DinamicArray_H
+#define DinamicArray_H
+
+template<class T>
+
+class DinamicArray{
+
+ private:
+  T * data;
+  int elements;
+  int max_elements;
+
+  void destroy();
+  
+ public:
+  DinamicArray<T>();
+  DinamicArray<T>(int n);
+  DinamicArray<T>(const DinamicArray<T>& original);
+  ~DinamicArray<T>();
+  int used() const;
+  int size() const;
+  T& operator [] (int i);
+  const T& operator [] (int i) const;
+  DinamicArray<T>& operator=(const DinamicArray<T>& original);
+  void push (T e);
+  void boost(int size);
+};
+
+#endif
